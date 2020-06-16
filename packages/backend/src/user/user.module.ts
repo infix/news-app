4bb59a5d483ac56bf34be41604cc359cc6ca8367
Amr Abulseoud <amrabulseoud@gmail.com>
2020-06-17 01:48:14 +0200

    [FEAT]: created user service

--- packages/backend/src/user/user.module.ts
+++ packages/backend/src/user/user.module.ts
@@ -1,4 +1,5 @@
 import { Module } from "@nestjs/common";
+import { UserService } from "./user.service";
 import { MongooseModule } from "@nestjs/mongoose";
 import { User, UserSchema } from "./user.schema";
 
@@ -6,5 +7,7 @@ import { User, UserSchema } from "./user.schema";
   imports: [
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
   ],
+  providers: [UserService],
+  exports: [UserService],
 })
 export class UserModule {}
---
+++ packages/backend/src/user/user.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from "@nestjs/common";
+import { InjectModel } from "@nestjs/mongoose";
+import { User } from "./user.schema";
+import { Model } from "mongoose";
+
+@Injectable()
+export class UserService {
+  constructor(
+    @InjectModel(User.name)
+    private userModel: Model<User>,
+  ) {}
+
+  async findByEmail(email: string): Promise<User | undefined> {
+    return this.userModel.findOne({ email });
+  }
+}
