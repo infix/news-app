6f96c04637510f983a3e931ab6f2a8da7a4ac56d
Amr Abulseoud <amrabulseoud@gmail.com>
2020-06-17 01:40:52 +0200

    [FEAT]: added graphql support with @nestjs/graphql

--- packages/backend/src/app.module.ts
+++ packages/backend/src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from "@nestjs/common";
 import { AppService } from "./app.service";
 import { MongooseModule } from "@nestjs/mongoose";
+import { GraphQLModule } from "@nestjs/graphql";
 
 @Module({
-  imports: [MongooseModule.forRoot("mongodb://localhost/news-app")],
-  controllers: [AppController],
+  imports: [
+    MongooseModule.forRoot("mongodb://localhost/news-app"),
+    GraphQLModule.forRoot({
+      autoSchemaFile: true,
+      playground: true,
+    }),
+  ],
+  controllers: [],
   providers: [AppService],
 })
 export class AppModule {}
