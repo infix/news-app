1e57843167a6a88bcddbdccb678648395adb409c
Amr Abulseoud <amrabulseoud@gmail.com>
2020-06-17 01:58:20 +0200

    [FEAT]: installed @nestjs/passport and @nestjs/jwt

--- packages/backend/src/auth/auth.module.ts
+++ packages/backend/src/auth/auth.module.ts
@@ -1,7 +1,15 @@
 import { Module } from "@nestjs/common";
 import { UserModule } from "../user/user.module";
+import { PassportModule } from "@nestjs/passport";
+import { JwtModule } from "@nestjs/jwt";
+import { AuthService } from './auth.service';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    UserModule,
+    PassportModule.register({ defaultStrategy: "jwt" }),
+    JwtModule.register({ secret: "Shh" }),
+  ],
+  providers: [AuthService],
 })
 export class AuthModule {}
