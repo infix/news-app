81cf6a6b3ec3a126f1a4e3e25024f14c98518518
Amr Abulseoud <amrabulseoud@gmail.com>
2020-06-18 00:17:02 +0200

    [REFACTOR]: updated userSchema to include a reference to the articleSchema

--- packages/backend/src/user/user.schema.ts
+++ packages/backend/src/user/user.schema.ts
@@ -1,5 +1,6 @@
 import * as mongoose from "mongoose";
-import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import { Prop, raw, Schema, SchemaFactory } from "@nestjs/mongoose";
+import { Article } from "../news/article.schema";
 
 @Schema()
 export class User extends mongoose.Document {
@@ -9,8 +10,15 @@ export class User extends mongoose.Document {
   @Prop()
   password: string;
 
-  @Prop()
-  favourites: String[];
+  @Prop(
+    raw([
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: Article.name,
+      },
+    ]),
+  )
+  favourites: Article[];
 }
 
 export const UserSchema = SchemaFactory.createForClass(User);
