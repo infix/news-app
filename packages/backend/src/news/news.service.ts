f5b50aa3a486215c948904cca242f9b6b70e2cfd
Amr Abulseoud <amrabulseoud@gmail.com>
2020-06-18 00:17:37 +0200

    [FEAT]: created a create method on the newsService

--- packages/backend/src/news/news.service.ts
+++ packages/backend/src/news/news.service.ts
@@ -2,6 +2,9 @@ import { Injectable } from "@nestjs/common";
 import { default as axios } from "axios";
 import { ConfigService } from "@nestjs/config";
 import { ArticleDTO } from "./article.dto";
+import { Article } from "./article.schema";
+import { InjectModel } from "@nestjs/mongoose";
+import { Model } from "mongoose";
 
 export type NewsCategory = "business" | "sports";
 export type NewsCountry = "eg" | "ae";
@@ -15,7 +18,11 @@ export class NewsService {
   private baseUrl = "http://newsapi.org/v2/top-headlines";
   private readonly apiKey: string;
 
-  constructor(private readonly config: ConfigService) {
+  constructor(
+    private readonly config: ConfigService,
+    @InjectModel(Article.name)
+    private readonly article: Model<Article>,
+  ) {
     this.apiKey = config.get<string>("NEWS_API_KEY");
   }
 
@@ -26,5 +33,9 @@ export class NewsService {
     const url = `${this.baseUrl}?country=${country}&category=${category}&apiKey=${this.apiKey}`;
     const newsResponse = await axios.get<any, NewsResponse>(url);
     return newsResponse.data.articles;
+  }
+
+  async create(article: ArticleDTO) {
+    return this.article.create(article);
   }
 }
