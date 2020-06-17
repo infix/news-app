1edcc7bff7cb43dae73627ccc3be86b8efea639b
Amr Abulseoud <amrabulseoud@gmail.com>
2020-06-18 00:52:38 +0200

    [FEAT]: installed react router, and created empty routes

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,5 +1,20 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-const App = () => <div>Hello World</div>;
+import { NewsPage } from "./pages/NewsPage";
+import { Login } from "./pages/Login";
+import { Register } from "./pages/Register";
+import { Favourites } from "./pages/Favourites";
+
+const App = () => (
+  <Router>
+    <Switch>
+      <Route path="/" exact render={NewsPage} />
+      <Route path="/login" render={Login} />
+      <Route path="/register" render={Register} />
+      <Route path="/favourites" render={Favourites} />
+    </Switch>
+  </Router>
+);
 
 export default App;
