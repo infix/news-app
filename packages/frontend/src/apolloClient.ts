37826dfd7774832a4c855876e76bf016a56ad724
Amr Abulseoud <amrabulseoud@gmail.com>
2020-06-18 03:51:01 +0200

    [FEAT]: updated apolloClient.ts to attach jwt token

--- packages/frontend/src/apolloClient.ts
+++ packages/frontend/src/apolloClient.ts
@@ -2,4 +2,12 @@ import ApolloClient from "apollo-boost";
 
 export const apolloClient = new ApolloClient({
   uri: "http://localhost:4000/graphql",
+  request: operation => {
+    const token = localStorage.getItem("token");
+    operation.setContext({
+      headers: {
+        authorization: token ? `Bearer ${token}` : "",
+      },
+    });
+  },
 });
