e3acab8338113d5986bf09dbe90d12a2a5dc7f02
Amr Abulseoud <amrabulseoud@gmail.com>
2020-06-18 05:21:41 +0200

    [FEAT]: Updated News feed to use the NewsFeedOptionsSelector, and NewsFeed components

--- packages/frontend/src/pages/NewsPage.tsx
+++ packages/frontend/src/pages/NewsPage.tsx
@@ -1,3 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
+import { Layout } from "antd";
+import { NewsFeedOptionsSelector } from "../components/NewsFeedOptionsSelector";
+import { NewsFeed } from "../components/NewsFeed";
 
-export const NewsPage = () => <>News Page</>;
+export const NewsPage = () => {
+  const [country, setCountry] = useState("eg");
+  const [category, setCategory] = useState("sports");
+
+  return (
+    <Layout style={{ minHeight: "100vh", alignItems: "center" }}>
+      <Layout.Content style={{ width: "40rem", padding: "1rem" }}>
+        {/* Not the cleanest implementation but this will do */}
+        <NewsFeedOptionsSelector
+          {...{ setCountry, setCategory, category, country }}
+        />
+        <NewsFeed category={category} country={country} />
+      </Layout.Content>
+    </Layout>
+  );
+};
