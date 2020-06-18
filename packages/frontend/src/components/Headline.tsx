f2e307648060b9427ae5e77e6039a0f6e05df22d
Amr Abulseoud <amrabulseoud@gmail.com>
2020-06-18 05:15:49 +0200

    [FEAT]: added add to favourites and remove from favourites functionality to Headline

--- packages/frontend/src/components/Headline.tsx
+++ packages/frontend/src/components/Headline.tsx
@@ -1,20 +1,64 @@
-import React from "react";
+import React, { useCallback, useMemo } from "react";
 import { Card, Typography } from "antd";
-import { Article } from "./NewsFeed";
-import { HeartOutlined, LinkOutlined } from "@ant-design/icons";
+import { Article, FAVOURITES } from "./NewsFeed";
+import { HeartOutlined, HeartTwoTone, LinkOutlined } from "@ant-design/icons";
+import { useMutation } from "@apollo/react-hooks";
+import { gql } from "apollo-boost";
 
-export const Headline: React.FC<{ article: Article }> = ({ article }) => {
+const ADD_TO_FAVOURITES = gql`
+  mutation ADD_TO_FAVOURITES($article: ArticleInput!) {
+    addToFavourites(article: $article)
+  }
+`;
+
+const REMOVE_FROM_FAVOURITES = gql`
+  mutation REMOVE_FROM_FAVOURITES($url: String!) {
+    removeFromFavourites(url: $url)
+  }
+`;
+
+type Props = { article: Article; isFavourite: boolean };
+export const Headline: React.FC<Props> = ({ article, isFavourite }) => {
+  const [addToFavMutation] = useMutation(ADD_TO_FAVOURITES);
+  const [removeFromFavMutation] = useMutation(REMOVE_FROM_FAVOURITES);
+
+  // Note: we should not use refetchQueries here since as
+  // it's always better to update the apollo cache.
+  // However, it doesn't matter in this case since if
+  // I update the cache the code would like a war crime
+
+  const addToFav = useCallback(() => {
+    const source = { ...article.source, __typename: undefined };
+    addToFavMutation({
+      variables: { article: { ...article, __typename: undefined, source } },
+      refetchQueries: [{ query: FAVOURITES }],
+    });
+  }, [article, addToFavMutation]);
+
+  const removeFromFav = useCallback(() => {
+    removeFromFavMutation({
+      variables: { url: article.url },
+      refetchQueries: [{ query: FAVOURITES }],
+    });
+  }, [article, removeFromFavMutation]);
+
+  const actions = useMemo(
+    () => [
+      isFavourite ? (
+        <HeartTwoTone twoToneColor="#eb2f96" onClick={removeFromFav} />
+      ) : (
+        <HeartOutlined onClick={addToFav} />
+      ),
+      <LinkOutlined onClick={() => window.open(article.url, "_blank")} />,
+    ],
+    [isFavourite, article],
+  );
+
   return (
     <Card
       cover={<img alt="example" src={article.urlToImage} />}
       style={{ width: "100%", margin: "0.5rem 0" }}
-      actions={[
-        <HeartOutlined key="heart" />,
-        <LinkOutlined
-          key="source"
-          onClick={() => window.open(article.url, "_blank")}
-        />,
-      ]}
+      actions={actions}
     >
       <Card.Meta
         title={<p style={{ textAlign: "center" }}>{article.title}</p>}
